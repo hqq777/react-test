a0473a1675cbb885b503b382d54578cd64a69b71
hqq777 <hqqqqqq1002@gmail.com>
2019-10-06 04:06:28 -0400

    Updates

---
+++ precache-manifest.3995ff2379c773d1af4b8e2e2b9b9552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bc9fc862dd78beacbd0d851e768fa2",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "1f06ffac6d8bc93d2e10",
+    "url": "/react-test/static/css/main.def2e945.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/react-test/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "1f06ffac6d8bc93d2e10",
+    "url": "/react-test/static/js/main.084cf2ef.chunk.js"
+  },
+  {
+    "revision": "b275cf5293c510d40d57",
+    "url": "/react-test/static/js/runtime-main.43f442a6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-test/static/media/logo.25bf045c.svg"
+  }
+]);
